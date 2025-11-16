907b3b38d01e3b407240a46e0b036aa123852904
emianuelul <emanuel.marin0000@gmail.com>
2025-11-16 16:26:06 +0200

    add digit test for capitalize

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -15,3 +15,8 @@ test('All Caps', () => {
   expect(capitalize('BANGDIK')).toMatch('BANGDIK');
   expect(capitalize('EMI')).toMatch('EMI');
 });
+
+test('Includes digits', () => {
+  expect(capitalize('123')).toMatch('123');
+  expect(capitalize('e@131')).toMatch('E@131');
+});
