7d5aac8064afaaf1db2b3cb4144341a05edb76fc
emianuelul <emanuel.marin0000@gmail.com>
2025-11-16 16:30:28 +0200

    finish reverseString + tests

--- functions/reverseString.js
+++ functions/reverseString.js
@@ -1,3 +1,10 @@
-function reverseString(string) {}
+function reverseString(string) {
+  let result = new String();
+  for (let i = string.length - 1; i >= 0; i--) {
+    result += string[i];
+  }
+
+  return result;
+}
 
 export { reverseString };
--- tests/reverseString.test.js
+++ tests/reverseString.test.js
@@ -3,3 +3,9 @@ import { reverseString } from '../functions/reverseString';
 test('Exists', () => {
   expect(reverseString).toBe;
 });
+
+test('Reverses', () => {
+  expect(reverseString('Emi')).toMatch('imE');
+  expect(reverseString('racecar')).toMatch('racecar');
+  expect(reverseString('1234567890')).toMatch('0987654321');
+});
