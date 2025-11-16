69ec56fcf2c77d36ce4cce7ace0f778b447213f3
emianuelul <emanuel.marin0000@gmail.com>
2025-11-16 19:18:24 +0200

    ceasarCipher function + tests

--- functions/caesarCipher.js
+++ functions/caesarCipher.js
@@ -1,3 +1,28 @@
-function caesarCipher(string, offset) {}
+function is_alphabetic(char) {
+  return (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z');
+}
+
+function caesarCipher(string, offset) {
+  let result = new String();
+
+  for (let i = 0; i < string.length; i++) {
+    if (!is_alphabetic(string[i])) {
+      result += string[i];
+      continue;
+    }
+    let oldCode = string.charCodeAt(i);
+    let newCode;
+
+    if (string[i].toLowerCase() === string[i]) {
+      newCode = 'a'.charCodeAt(0) + ((oldCode - 'a'.charCodeAt(0) + offset) % 26);
+    } else if (string[i].toUpperCase() === string[i]) {
+      newCode = 'A'.charCodeAt(0) + ((oldCode - 'A'.charCodeAt(0) + offset) % 26);
+    }
+
+    result += String.fromCharCode(newCode);
+  }
+
+  return result;
+}
 
 export { caesarCipher };
--- tests/caesarCipher.test.js
+++ tests/caesarCipher.test.js
@@ -3,3 +3,21 @@ import { caesarCipher } from '../functions/caesarCipher';
 test('Exists', () => {
   expect(caesarCipher).toBe;
 });
+
+test('works for abc', () => {
+  expect(caesarCipher('abc', 3)).toBe('def');
+});
+
+test('z-a wrapping', () => {
+  expect(caesarCipher('xyz', 3)).toBe('abc');
+});
+
+test('Case preservation', () => {
+  expect(caesarCipher('HeLLo', 3)).toBe('KhOOr');
+  expect(caesarCipher('AbCz', 2)).toBe('CdEb');
+});
+
+test('Ignores non letter characters', () => {
+  expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!');
+  expect(caesarCipher('123! go.', 5)).toBe('123! lt.');
+});
