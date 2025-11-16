51187b337819886376f5a340c16a0ea1335aca11
emianuelul <emanuel.marin0000@gmail.com>
2025-11-16 17:50:23 +0200

    analyzeArray function + tests

--- functions/analyzeArray.js
+++ functions/analyzeArray.js
@@ -1,3 +1,17 @@
-function analyzeArray(array) {}
+function analyzeArray(array) {
+  let average = array.reduce((prev, curr) => prev + curr, 0);
+  average = average / array.length;
+
+  let min = Math.min(...array);
+  let max = Math.max(...array);
+  let length = array.length;
+
+  return {
+    average: average,
+    min: min,
+    max: max,
+    length: length,
+  };
+}
 
 export { analyzeArray };
--- tests/analyzeArray.test.js
+++ tests/analyzeArray.test.js
@@ -3,3 +3,39 @@ import { analyzeArray } from '../functions/analyzeArray';
 test('Exists', () => {
   expect(analyzeArray).toBe;
 });
+
+test('Basic array', () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toMatchObject({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
+});
+
+test('Single element array', () => {
+  expect(analyzeArray([5])).toMatchObject({
+    average: 5,
+    min: 5,
+    max: 5,
+    length: 1,
+  });
+});
+
+test('Negative numbers', () => {
+  expect(analyzeArray([-2, -8, -3, -4])).toMatchObject({
+    average: -4.25,
+    min: -8,
+    max: -2,
+    length: 4,
+  });
+});
+
+test('Mixed numbers', () => {
+  expect(analyzeArray([10, -3, 0, 7, 2])).toMatchObject({
+    average: 3.2,
+    min: -3,
+    max: 10,
+    length: 5,
+  });
+});
